ae772d3c6d83f0de53a67c7ee72ab71bfbde6108
yadubuilds <mail@yadusins.com>
2026-01-16 04:56:17 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,22 @@
 module.exports = {
   apps: [
-    { name: "userbot", script: "userbot/main.py", interpreter: "python3" },
-    { name: "control-bot", script: "bot/main.py", interpreter: "python3" }
+    {
+      name: "control-bot",
+      script: "bot/main.py",
+      interpreter: "python3",
+      cwd: "/root/pyrogram-control-userbot",
+      env: {
+        PYTHONPATH: "."
+      }
+    },
+    {
+      name: "userbot",
+      script: "userbot/main.py",
+      interpreter: "python3",
+      cwd: "/root/pyrogram-control-userbot",
+      env: {
+        PYTHONPATH: "."
+      }
+    }
   ]
 }
